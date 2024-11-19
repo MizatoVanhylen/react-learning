a92c92d30273d5641de969750ebd91c029cd6066
MizatoVanhylen <denisse.romero.c@gmail.com>
2024-11-19 17:42:44 -0300

    se agrega ejercicio de conteno de caracteres

--- src/Components/Sailor.jsx
+++ src/Components/Sailor.jsx
@@ -12,18 +12,19 @@ const Sailor = ({
   }) => {
   const leo = signozodiacal == "Leo" ? <p className="parrafo"> <strong>Estado:</strong> Deprimida </p> : <p>no eres leo</p>
   const mayorDeEdad = edad > 18 ? "?" : `${edad} años`;
+  const mayorOcho = nombre.length > 8 ? "..." : `${nombre}`; 
   return(
-    <div className="inicial">
-      <h1>Personaje {personaje}</h1>
+    <div className="inicial" contenido>
+      <h1>Personaje {personaje.toUpperCase()}</h1>
       <div className="descrition" >
         <p> Descripción de personaje: </p>
           <div className="info">
-            <p className="parrafo"> <strong>Nombre:</strong> {nombre}</p>
-            { signozodiacal != "Leo" && <p className="parrafo"> <strong>Cumpleaños:</strong> {cumpleaños}</p> }
-            { <p className="parrafo"> <strong>Edad:</strong> {mayorDeEdad} </p> }
-            <p className="parrafo"> <strong>Signo Zodiacal:</strong> {signozodiacal}</p>
+            <p className="parrafo"> <strong>Nombre:</strong> {mayorOcho.toUpperCase()}</p>
+            { signozodiacal != "Leo" && <p className="parrafo"> <strong>Cumpleaños:</strong> {cumpleaños.toUpperCase()}</p> }
+            { <p className="parrafo"> <strong>Edad:</strong> {mayorDeEdad.toUpperCase()} </p> }
+            <p className="parrafo"> <strong>Signo Zodiacal:</strong> {signozodiacal.toUpperCase()}</p>
             <p className="parrafo"> <strong>Altura:</strong> {altura} cm</p>
-            { signozodiacal != "Leo" && <p className="parrafo"> <strong>Planeta:</strong> {planeta}</p> }
+            { signozodiacal != "Leo" && <p className="parrafo"> <strong>Planeta:</strong> {planeta.toUpperCase()}</p> }
             {leo}
           </div>
       </div>
